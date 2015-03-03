434ae22ef9b099dbd4e9b4a30e170b73422bf7dc
Espen Moe <espen357moe@gmail.com>
2015-03-03 15:27:27 +0100

    Lagde en utgangs-Form

--- Booking/Form1.Designer.cs
+++ Booking/Form1.Designer.cs
@@ -28,12 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameInputBox = new System.Windows.Forms.TextBox();
+            this.gjesteListe = new System.Windows.Forms.ListBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.bookingListe = new System.Windows.Forms.ListBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 615);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Logo her";
+            // 
+            // nameInputBox
+            // 
+            this.nameInputBox.Location = new System.Drawing.Point(16, 12);
+            this.nameInputBox.Name = "nameInputBox";
+            this.nameInputBox.Size = new System.Drawing.Size(410, 22);
+            this.nameInputBox.TabIndex = 1;
+            // 
+            // gjesteListe
+            // 
+            this.gjesteListe.FormattingEnabled = true;
+            this.gjesteListe.ItemHeight = 16;
+            this.gjesteListe.Location = new System.Drawing.Point(16, 47);
+            this.gjesteListe.Name = "gjesteListe";
+            this.gjesteListe.Size = new System.Drawing.Size(199, 340);
+            this.gjesteListe.TabIndex = 2;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(432, 9);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(609, 702);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(601, 673);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "1. etg";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(595, 673);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "2. etg";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(595, 673);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "3. etg";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // bookingListe
+            // 
+            this.bookingListe.FormattingEnabled = true;
+            this.bookingListe.ItemHeight = 16;
+            this.bookingListe.Location = new System.Drawing.Point(227, 47);
+            this.bookingListe.Name = "bookingListe";
+            this.bookingListe.Size = new System.Drawing.Size(199, 660);
+            this.bookingListe.TabIndex = 4;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(16, 399);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 723);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.bookingListe);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.gjesteListe);
+            this.Controls.Add(this.nameInputBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameInputBox;
+        private System.Windows.Forms.ListBox gjesteListe;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.ListBox bookingListe;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
     }
 }
 
---
+++ Booking/HotellRom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Booking
+{
+    class HotellRom : Panel
+    {
+        public int      romNummer;
+        public string   GjesteNavn;
+        public bool     opptatt;
+
+    }
+}
