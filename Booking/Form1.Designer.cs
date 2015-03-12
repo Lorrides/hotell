555b5812bc6fb49df2462c771a63b2c098ff93b0
Lorrides <tarhh@outlook.com>
2015-03-12 14:24:23 +0100

    Laget metode for lagring av gjeste navn

--- Booking/Form1.Designer.cs
+++ Booking/Form1.Designer.cs
@@ -31,107 +31,128 @@
             this.label1 = new System.Windows.Forms.Label();
             this.nameInputBox = new System.Windows.Forms.TextBox();
             this.gjesteListe = new System.Windows.Forms.ListBox();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.bookingListe = new System.Windows.Forms.ListBox();
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 615);
+            this.label1.Location = new System.Drawing.Point(10, 500);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.Size = new System.Drawing.Size(49, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Logo her";
             // 
             // nameInputBox
             // 
-            this.nameInputBox.Location = new System.Drawing.Point(16, 12);
+            this.nameInputBox.Location = new System.Drawing.Point(12, 10);
+            this.nameInputBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.nameInputBox.Multiline = true;
             this.nameInputBox.Name = "nameInputBox";
-            this.nameInputBox.Size = new System.Drawing.Size(410, 22);
+            this.nameInputBox.Size = new System.Drawing.Size(207, 29);
             this.nameInputBox.TabIndex = 1;
             // 
             // gjesteListe
             // 
             this.gjesteListe.FormattingEnabled = true;
-            this.gjesteListe.ItemHeight = 16;
-            this.gjesteListe.Location = new System.Drawing.Point(16, 47);
+            this.gjesteListe.Location = new System.Drawing.Point(12, 77);
+            this.gjesteListe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gjesteListe.Name = "gjesteListe";
-            this.gjesteListe.Size = new System.Drawing.Size(199, 340);
+            this.gjesteListe.Size = new System.Drawing.Size(220, 238);
             this.gjesteListe.TabIndex = 2;
+            // 
+            // bookingListe
+            // 
+            this.bookingListe.FormattingEnabled = true;
+            this.bookingListe.Location = new System.Drawing.Point(241, 84);
+            this.bookingListe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bookingListe.Name = "bookingListe";
+            this.bookingListe.Size = new System.Drawing.Size(183, 485);
+            this.bookingListe.TabIndex = 4;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(12, 324);
+            this.monthCalendar1.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 6;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage3.Size = new System.Drawing.Size(680, 544);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "3. etg";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Size = new System.Drawing.Size(680, 544);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "2. etg";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Location = new System.Drawing.Point(432, 9);
+            this.tabControl1.Location = new System.Drawing.Point(428, 10);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(609, 702);
+            this.tabControl1.Size = new System.Drawing.Size(584, 570);
             this.tabControl1.TabIndex = 3;
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(601, 673);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(576, 544);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "1. etg";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // button1
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(595, 673);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "2. etg";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tabPage3
-            // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 25);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(595, 673);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "3. etg";
-            this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // bookingListe
-            // 
-            this.bookingListe.FormattingEnabled = true;
-            this.bookingListe.ItemHeight = 16;
-            this.bookingListe.Location = new System.Drawing.Point(227, 47);
-            this.bookingListe.Name = "bookingListe";
-            this.bookingListe.Size = new System.Drawing.Size(199, 660);
-            this.bookingListe.TabIndex = 4;
-            // 
-            // monthCalendar1
-            // 
-            this.monthCalendar1.Location = new System.Drawing.Point(16, 399);
-            this.monthCalendar1.Name = "monthCalendar1";
-            this.monthCalendar1.TabIndex = 6;
+            this.button1.Location = new System.Drawing.Point(241, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 29);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Legg til ny gjest";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1053, 723);
+            this.ClientSize = new System.Drawing.Size(1023, 606);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.bookingListe);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.gjesteListe);
             this.Controls.Add(this.nameInputBox);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
@@ -145,12 +166,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox nameInputBox;
         private System.Windows.Forms.ListBox gjesteListe;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.ListBox bookingListe;
         private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage2;
+        internal System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Booking/Form1.cs
+++ Booking/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Booking
 {
@@ -15,6 +16,23 @@ namespace Booking
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                XDocument doc = XDocument.Load("../../XML/nyGjest.XML");
+                doc.Root.Add(
+                    new XElement("gjest",
+                    new XElement("gjestenavn", nameInputBox.Text)));
+                doc.Save("../../XML/nyGjest.XML");
+
+            }
+            catch (Exception f)
+            {
+                MessageBox.Show("Noe gikk galt:" + f.Message);
+            }
         }
     }
 }
