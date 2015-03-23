f099664b97f930f438f6a86f86ea4569b412bf42
Lorrides <tarhh@outlook.com>
2015-03-23 13:58:09 +0100

    Endret visning av ny gjest i Design

--- Booking/Form1.Designer.cs
+++ Booking/Form1.Designer.cs
@@ -37,29 +37,31 @@
             this.innSjekkDatoLabel = new System.Windows.Forms.Label();
             this.utSjekkDatoLabel = new System.Windows.Forms.Label();
             this.utsjekkDatoVelger = new System.Windows.Forms.DateTimePicker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // nameInputBox
             // 
-            this.nameInputBox.Location = new System.Drawing.Point(12, 10);
+            this.nameInputBox.Location = new System.Drawing.Point(38, 22);
             this.nameInputBox.Margin = new System.Windows.Forms.Padding(2);
             this.nameInputBox.Name = "nameInputBox";
-            this.nameInputBox.Size = new System.Drawing.Size(207, 20);
+            this.nameInputBox.Size = new System.Drawing.Size(143, 20);
             this.nameInputBox.TabIndex = 1;
             // 
             // gjesteListe
             // 
             this.gjesteListe.FormattingEnabled = true;
-            this.gjesteListe.Location = new System.Drawing.Point(11, 71);
+            this.gjesteListe.Location = new System.Drawing.Point(13, 193);
             this.gjesteListe.Margin = new System.Windows.Forms.Padding(2);
             this.gjesteListe.Name = "gjesteListe";
-            this.gjesteListe.Size = new System.Drawing.Size(206, 355);
+            this.gjesteListe.Size = new System.Drawing.Size(206, 394);
             this.gjesteListe.TabIndex = 2;
             // 
             // bookingListe
             // 
             this.bookingListe.FormattingEnabled = true;
-            this.bookingListe.Location = new System.Drawing.Point(223, 19);
+            this.bookingListe.Location = new System.Drawing.Point(223, 11);
             this.bookingListe.Margin = new System.Windows.Forms.Padding(2);
             this.bookingListe.Name = "bookingListe";
             this.bookingListe.Size = new System.Drawing.Size(201, 576);
@@ -76,9 +78,9 @@
             // 
             // leggTilNyGjest
             // 
-            this.leggTilNyGjest.Location = new System.Drawing.Point(12, 35);
+            this.leggTilNyGjest.Location = new System.Drawing.Point(38, 147);
             this.leggTilNyGjest.Name = "leggTilNyGjest";
-            this.leggTilNyGjest.Size = new System.Drawing.Size(207, 29);
+            this.leggTilNyGjest.Size = new System.Drawing.Size(143, 29);
             this.leggTilNyGjest.TabIndex = 7;
             this.leggTilNyGjest.Text = "Legg til ny gjest";
             this.leggTilNyGjest.UseVisualStyleBackColor = true;
@@ -87,17 +89,18 @@
             // innsjekkDatoVelger
             // 
             this.innsjekkDatoVelger.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.innsjekkDatoVelger.Location = new System.Drawing.Point(11, 521);
+            this.innsjekkDatoVelger.Location = new System.Drawing.Point(55, 71);
             this.innsjekkDatoVelger.Name = "innsjekkDatoVelger";
-            this.innsjekkDatoVelger.Size = new System.Drawing.Size(200, 20);
+            this.innsjekkDatoVelger.Size = new System.Drawing.Size(98, 20);
             this.innsjekkDatoVelger.TabIndex = 8;
             this.innsjekkDatoVelger.ValueChanged += new System.EventHandler(this.innSjekkDato_ValueChanged);
             // 
             // innSjekkDatoLabel
             // 
             this.innSjekkDatoLabel.AutoSize = true;
+            this.innSjekkDatoLabel.BackColor = System.Drawing.Color.LightSkyBlue;
             this.innSjekkDatoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.innSjekkDatoLabel.Location = new System.Drawing.Point(27, 494);
+            this.innSjekkDatoLabel.Location = new System.Drawing.Point(34, 44);
             this.innSjekkDatoLabel.Name = "innSjekkDatoLabel";
             this.innSjekkDatoLabel.Size = new System.Drawing.Size(154, 24);
             this.innSjekkDatoLabel.TabIndex = 9;
@@ -106,8 +109,9 @@
             // utSjekkDatoLabel
             // 
             this.utSjekkDatoLabel.AutoSize = true;
+            this.utSjekkDatoLabel.BackColor = System.Drawing.Color.LightSkyBlue;
             this.utSjekkDatoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.utSjekkDatoLabel.Location = new System.Drawing.Point(27, 544);
+            this.utSjekkDatoLabel.Location = new System.Drawing.Point(34, 94);
             this.utSjekkDatoLabel.Name = "utSjekkDatoLabel";
             this.utSjekkDatoLabel.Size = new System.Drawing.Size(145, 24);
             this.utSjekkDatoLabel.TabIndex = 10;
@@ -116,11 +120,20 @@
             // utsjekkDatoVelger
             // 
             this.utsjekkDatoVelger.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.utsjekkDatoVelger.Location = new System.Drawing.Point(12, 571);
+            this.utsjekkDatoVelger.Location = new System.Drawing.Point(55, 121);
             this.utsjekkDatoVelger.Name = "utsjekkDatoVelger";
-            this.utsjekkDatoVelger.Size = new System.Drawing.Size(200, 20);
+            this.utsjekkDatoVelger.Size = new System.Drawing.Size(98, 20);
             this.utsjekkDatoVelger.TabIndex = 11;
             this.utsjekkDatoVelger.ValueChanged += new System.EventHandler(this.utsjekkDatoVelger_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(206, 177);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
@@ -136,9 +149,11 @@
             this.Controls.Add(this.etasjeOversiktTabControl);
             this.Controls.Add(this.gjesteListe);
             this.Controls.Add(this.nameInputBox);
+            this.Controls.Add(this.pictureBox1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,6 +170,7 @@
         private System.Windows.Forms.Label innSjekkDatoLabel;
         private System.Windows.Forms.Label utSjekkDatoLabel;
         private System.Windows.Forms.DateTimePicker utsjekkDatoVelger;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
