d80e9ba968ff4850273000ef307cd795d9278d7e
Espen Moe <espen357moe@gmail.com>
2015-04-05 16:07:25 +0200

    Gjorde opp på strukturen for å tegne rom. Gjenstår fortsatt en del

--- Booking/Form1.cs
+++ Booking/Form1.cs
@@ -34,14 +34,42 @@ namespace Booking
                 etasjeOversiktTabControl.TabPages[i].Controls.Add(new FlowLayoutPanel());              
             }
 
+            int etasjeTeller = 0;
+
             foreach (Etasje etasje in hotell._etasjer)
-            {
-                for (int i = 0; i < antallRomPerEtasje; i++)
-                {                            
-                    etasje.AddRom();
-                    //(etasjeOversiktTabControl.TabPages[i].Controls[0] as FlowLayoutPanel).Controls.Add(new Label());
-                }                  
-            }           
+            {                              
+                for (int i = 1; i <= 3; i++)
+                {                    
+                    for (int j = 1; j <= 4; j++)
+                    {
+                        Panel a = new Panel();
+                        a.Location = new Point(i * 200, j * 50);
+                        a.Width = 180;
+                        a.Height = 40;
+                        a.Name = "Rom " + (((i * 4) - 3) + (j - 1));
+                        a.BackColor = Color.Yellow;
+                        a.AllowDrop = true;
+                        // Setter opp hendelseshåndterere for DragOver og DragDrop
+                         //a.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+                         //a.DragOver += new System.Windows.Forms.DragEventHandler(this.panel1_DragOver);
+                        a.Visible = true;
+                        // Legger til en label inne i panelet
+                        Label l = new Label();
+                        l.Location = new Point(10, 10);
+                        l.Width = 180;
+                        l.Text = a.Name;
+                        a.Controls.Add(l);
+                        // Legger panelet til form
+                        etasjeOversiktTabControl.TabPages[etasjeTeller].Controls.Add(a);
+
+                    }
+                    
+                }
+                etasjeTeller++;
+            } 
+          
+
+
         }
 
         XmlHåndterer xh = new XmlHåndterer();
